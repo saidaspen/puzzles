f279c404a947ad6af1ccfd74a7901464d7107308
Said Aspen <mail@saidaspen.se>
2020-12-31 01:50:47 +0100

    Works!

--- codewars/codewars-kotlin/src/main/kotlin/kata/Cryptarithm.kt
+++ codewars/codewars-kotlin/src/main/kotlin/kata/Cryptarithm.kt
@@ -4,9 +4,9 @@ object Cryptarithm {
     private val allDigits = (0..9).toList()
 
     fun alphametics(puzzle: String): String {
-        val words = puzzle.split(" ").filter { it.toCharArray().all { l -> l.isLetter() } }
+        val words = puzzle.split(" ").filter { it.toCharArray().all { l -> l.isLetter() } }.map { it.toCharArray() }
         val firstChars = words.map { it.first() }
-        val numLetters = words.flatMap { it.toCharArray().toList() }.distinct().size
+        val numLetters = words.flatMap { it.toList() }.distinct().size
 
         fun isValid(assignment: Map<Char, Int>) =
             words.dropLast(1).map { transform(it, assignment).toInt() }.sum() == transform(
@@ -15,7 +15,7 @@ object Cryptarithm {
             ).toInt()
 
         fun search(idx: Int, assignment: Map<Char, Int>, carry: Int): Map<Char, Int>? {
-            val charsAtIdx = words.map { it.toCharArray() }.filter { it.size > idx }.map { it[it.size - idx - 1] }
+            val charsAtIdx = words.filter { it.size > idx }.map { it[it.size - idx - 1] }
             if (numLetters == assignment.size) return if (isValid(assignment)) assignment else null
             val candidates = solve(charsAtIdx, assignment, carry).filter { firstChars.none { c -> it.first[c] == 0 } }
             for (candidate in candidates) {
@@ -27,14 +27,13 @@ object Cryptarithm {
         val initialAssignments = mutableMapOf<Char, Int>()
         val lastChars = words.map { it.last() }.distinct()
         if (lastChars.size == 1) initialAssignments[lastChars[0]] = 0
-        if (words.size == 2 && words.last().length > words.dropLast(1).map { it.length }.maxOrNull()!!){
+        if (words.size == 2 && words.last().size > words.dropLast(1).map { it.size }.max()!!){
             initialAssignments[words.last()[0]] = 1
         }
-        return transform(puzzle, search(0, initialAssignments, 0)!!)
+        return transform(puzzle.toCharArray(), search(0, initialAssignments, 0)!!)
     }
 
-    private fun transform(s: String, assignments: Map<Char, Int>) =
-        s.toCharArray().map { assignments[it]?.toString() ?: it }.joinToString("")
+    private fun transform(s: CharArray, assignments: Map<Char, Int>) = s.map { assignments[it]?.toString() ?: it }.joinToString("")
 
     private fun solve(letters: List<Char>, assignment: Map<Char, Int>, carry: Int): List<Pair<Map<Char, Int>, Int>> {
         val unassignedLetters = letters.distinct().filter { !assignment.keys.contains(it) }
@@ -45,7 +44,6 @@ object Cryptarithm {
         return allDigits
             .filter { !assignment.values.contains(it) }
             .combinations(unassignedLetters.size)
-            .filter { it.none { d -> it.count { od -> od == d } > 1 } }
             .map { cand -> letters.map { assignment[it] ?: cand[unassignedLetters.indexOf(it)] } }
             .filter { (it.dropLast(1).sum() + carry) % 10 == it.last() }
             .map {
@@ -60,6 +58,7 @@ private fun List<Int>.combinations(n: Int): List<List<Int>> {
         if (result.size == l) return mutableListOf(result)
         val combinations = mutableListOf<List<Int>>()
         for (i in a.indices) {
+            if (result.contains(a[i])) continue
             val nr = result + a[i]
             combinations.addAll(perm(a, nr, l))
         }
